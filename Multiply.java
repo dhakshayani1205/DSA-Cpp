5c40eb54265d61e7456efb2a6f3d03ddb21cff4a
dhakshayani1205 <kelamdhakshayani@gmail.com>
2025-07-20 14:58:38 +0530

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,13 @@
+//a java program to multiply two integer values
+class Multiply{
+public static void main(String n[]){
+int a,b;
+a=2;
+b=4;
+int c=a*b;
+//System.out.print("Result="=c);
+System.out.print("product of"+a+"and"+b+"is"+c);
+}
+}
+
+ 
